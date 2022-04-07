90b4ca30117667f5c3288d5d0806eb1072dcb6b1
Sbaoor <2959435045@qq.com>
2022-04-07 20:44:29 +0800

    二次提交

---
+++ bats/pack.bat
@@ -0,0 +1,36 @@
+chcp 65001
+@echo off
+set /p v=请输入NilBridge版本号：
+
+echo [%date% %time%] 开始打包Release_%v%.zip >> logs.txt
+
+cd ../
+
+if not exist node/node.exe (echo 无法找到node.exe，请把node.exe放到node文件夹 && pause && exit)
+
+if not exist Release_%v% md Release_%v%
+
+echo 正在复制文件...请稍候
+
+: 如果要复制.git文件夹就加上 /h 参数
+xcopy nilbridge Release_%v% /e /i /Q /y /exclude:%cd%\configs\pack.config
+
+echo 正在复制node.exe
+
+copy node/node.exe Release_%v%/node.exe
+
+echo 正在压缩...
+
+cd 7z
+
+7za.exe a ../Release_%v%.zip  ../Release_%v%
+
+cd ../
+
+echo 压缩成功，请确认是否删除Release_%v%文件夹
+
+rmdir Release_%v% /s
+
+echo [%date% %time%] 打包了Release_%v% >> logs.txt
+
+pause
---
+++ install.bat
@@ -0,0 +1,5 @@
+git clone https://github.com/nilbridge/nilbridge2.git NilBridge
+
+echo 仓库克隆成功 >> logs.txt
+
+pause
