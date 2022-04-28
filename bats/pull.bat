7bfb37a1cbcf5abdd03aa204a27eceb7a1435b24
Sbaoor <2959435045@qq.com>
2022-04-28 22:42:51 +0800

    update

--- bats/pull.bat
+++ bats/pull.bat
@@ -1,3 +1,5 @@
+chcp 65001
+
 cd ../NilBridge
 
 echo [%date% %time%] 正在拉取最新版
--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 git clone https://github.com/nilbridge/nilbridge2.git NilBridge
 
-echo 仓库克隆成功 >> logs.txt
+echo [%date% %time%]仓库克隆成功 >> logs.txt
 
 md node
 
